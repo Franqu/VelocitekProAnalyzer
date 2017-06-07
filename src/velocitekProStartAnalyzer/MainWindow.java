c08fff2c16eec59addfd72bb379d64ab85c92238
Franqu <maciejedrzejko@gmail.com>
2017-06-08 01:35:20 +0200

    Dodano Dialog to wstawienia godziny startu

--- src/velocitekProStartAnalyzer/MainWindow.java
+++ src/velocitekProStartAnalyzer/MainWindow.java
@@ -5,14 +5,18 @@ import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Vector;
 
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
@@ -169,7 +173,7 @@ public class MainWindow {
 		btnPanel.add(btnSetStartTime);
 		btnSetStartTime.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				setStartTime("14:00");
+				setStartTime(showSetTimerDialog());
 				loadDataFromDB();
 			}
 		});
@@ -293,8 +297,66 @@ public class MainWindow {
   // btnPanel.add(showFileDialogButton);
      
 }
-	
+	private String showSetTimerDialog(){
+		//Object[] possibilities = {"ham", "spam", "yam"};
+		String s = (String)JOptionPane.showInputDialog(
+            frame,
+            "Choose start time:\n"
+            + "\"Green eggs and...\"",
+            "Customized Dialog",
+            JOptionPane.PLAIN_MESSAGE,
+            null,
+            null,
+            "00:00");
+		return s;
 
+		}
+	/*private void showSetTimerTEST(){
+		final JOptionPane optionPane = new JOptionPane(
+                "The only way to close this dialog is by\n"
+                + "pressing one of the following buttons.\n"
+                + "Do you understand?",
+                JOptionPane.QUESTION_MESSAGE,
+                JOptionPane.YES_NO_OPTION);
+
+					final JDialog dialog = new JDialog(frame, 
+					                             "Click a button",
+					                             true);
+					dialog.setContentPane(optionPane);
+					dialog.setDefaultCloseOperation(
+					    JDialog.DO_NOTHING_ON_CLOSE);
+					dialog.addWindowListener(new WindowAdapter() {
+					    public void windowClosing(WindowEvent we) {
+					        setLabel("Thwarted user attempt to close window.");
+					    }
+					});
+					optionPane.addPropertyChangeListener(
+					    new PropertyChangeListener() {
+					        public void propertyChange(PropertyChangeEvent e) {
+					            String prop = e.getPropertyName();
+					
+					            if (dialog.isVisible() 
+					             && (e.getSource() == optionPane)
+					             && (prop.equals(JOptionPane.VALUE_PROPERTY))) {
+					                //If you were going to check something
+					                //before closing the window, you'd do
+					                //it here.
+					                dialog.setVisible(false);
+					            }
+					        }
+					    });
+					dialog.pack();
+					dialog.setVisible(true);
+					
+					int value = ((Integer)optionPane.getValue()).intValue();
+					if (value == JOptionPane.YES_OPTION) {
+					    setLabel("Good.");
+					} else if (value == JOptionPane.NO_OPTION) {
+					    setLabel("Try using the window decorations "
+					             + "to close the non-auto-closing dialog. "
+					             + "You can't!");
+					}
+	}*/
 
 public JScrollPane getScrollTable() {
 	return scrollTable;
